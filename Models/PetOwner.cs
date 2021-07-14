d0d8a6eea41a016fe771d584e894ff3628f1a79d
Chris Klemz <crklemz@gmail.com>
2021-07-14 15:12:34 -0500

    Pet and PetOwner models complete

---
+++ Models/PetOwner.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System;
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace pet_hotel
+{
+    public class PetOwner {
+
+        public int id {get; set;}
+
+        [Required]
+        public string name {get; set;}
+
+        public string emailAddress {get; set;}
+
+        public int petCount {get; set;}
+
+    }
+}
